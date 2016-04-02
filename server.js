a711fefa0bf441ee15956d81aaecceaf3a7069df
aca12uke <ukeze1@sheffield.ac.uk>
2016-04-02 14:49:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/**
+ * Created by fabio on 18/02/15.
+ */
 var protocol = require('http');
 var static = require('node-static');
 var util = require('util');
@@ -6,113 +9,70 @@ var querystring = require('querystring');
 var Twit = require('twit');
 
 
-
 var client = new Twit({
-  consumer_key: 'QQY9a1KOfB9f9yrDu5TPYNxM1',
-  consumer_secret: 'KFcrYRVEAou07gQwrjNUmHUsJInLa7kz8TdeCMaxSjC3sD0EEM',
-  access_token: '312094689-JpmZ9EJwxUUmZErdzTbagA5zlnndyyXj4ckOVQtE',
-  access_token_secret: 'S6U36toKMU9FZNriCh2MqM4ZbYzbnzQ65l8w6m0ydeqEe'
+  consumer_key: 'p3z3Uya200JQo6itVOOWixkW8',
+  consumer_secret: 'wXMbKhs2OVsCi5UUQaJAIHo2fLR2KGy42FbzZ8vCWsPqsXxBIg',
+  access_token: '277462049-YNaK9sdOazCkG6XrltVWUgynV1olfnMlqPfKTDQm',
+  access_token_secret: 'hQyUBW1MGd2fryHinjqQNrQZjybsoSw5zevwLxIaicHgo'
 });
 
+ var file = new (static.Server)();
+  var portNo = 3001;
+  var app = protocol.createServer(function (req, res) {
+      var pathname = url.parse(req.url).pathname;
+      if ((req.method == 'POST') && (pathname == '/postFile.html')) {
+          var body = '';
+          req.on('data', function (data) {
+              body += data;
+              // if body >  1e6 === 1 * Math.pow(10, 6) ~~~ 1MB
+              // flood attack or faulty client
+              // (code 413: req entity too large), kill req
+              if (body.length > 1e6) {
+                  res.writeHead(413,
+                      {'Content-Type': 'text/plain'}).end();
+                  req.connection.destroy();
+              }
+          });
+          req.on('end', function () {
+              var string = JSON.parse(body);
+              res.writeHead(200, {"Content-Type": "application/json"});
+             //res.write(body);
 
-var file = new (static.Server)();
-var portNo = 3001;
-var app = protocol.createServer(function (req, res) {
-    var pathname = url.parse(req.url).pathname;
-    if ((req.method == 'POST') && (pathname == '/postFile.html')) {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            // if body >  1e6 === 1 * Math.pow(10, 6) ~~~ 1MB
-            // flood attack or faulty client
-            // (code 413: req entity too large), kill req
-            if (body.length > 1e6) {
-                res.writeHead(413,
-                    {'Content-Type': 'text/plain'}).end();
-                req.connection.destroy();
-            }
-        });
-        req.on('end', function () {
-            var string = JSON.parse(body);
-            res.writeHead(200, {"Content-Type": "application/json"});
-            //res.write(body);
-            //+console.log(body);
-
-           var tweetsA = [];
-           var jsonx = {};
-            client.get('search/tweets', { q: string.teamname, count: 1},
-                        function searchTweets(err, data, response) {
-                        for(var index in data.statuses){
-                             var tweet = data.statuses[index]; 
-                               tweetsA.push(tweet.text.split(" "));
-                        }
-                    /* Callback function to query Twitter for statuses*/
-            client.get('statuses/user_timeline', { screen_name: string.teamname, count: 10},
-                        function listStatuses (err, data, response) {
-                          for(var index in data){
-                               var tweet = data[index]; 
-                                  tweetsA.push(tweet.text.split(" "));
-                          }
+              jsonx = {};
 
 
-                            //jsonx[0]= tweetsA;
-                            //console.log(jsonx);
-                            //res.end(JSON.stringify(jsonx[0]));
-
-                            var regex = /\B@\w+/gi
-                            var listsb = [];
-                            var dict = {};
-
-                                  for ( i = 0 ; i < tweetsA.length; i++) {
-                                     for(j = 0; j < tweetsA[i].length; j++){
-                                        while((m = regex.exec(tweetsA[i][j])) != null) {
-                                           listsb = listsb.concat(m[0]);
-                                        }
-                                          
-                                        
-                                         
-
-                                      }
-                                  }  
-
-                                   
-
-                                  for (i = 0; i < listsb.length; i++){
-                                    if (listsb[i] in dict){
-                                       dict[listsb[i]] += 1
-                                    }else{
-                                       dict[listsb[i]] = 1
-                                    }   
-                                    
-                                  }
+              client.get('search/tweets', { q:string.teamname, count:13},
+              function searchTweets(err, data, response) {
+                for(var index in data.statuses){
+                  var tweet = data.statuses[index];
 
 
+                  jsonx[index] = tweet;
 
-                                    console.log(dict)                                     
-                   
-                      
+
+                }
+                console.log(jsonx);
+                res.end(JSON.stringify(jsonx));
               });
 
-            
-      });
-      });
-      
+        });
    }
 
-    else {
-        file.serve(req, res, function (err, result) {
-            if (err != null) {
-                console.error('Error serving %s - %s', req.url, err.message);
-                if (err.status === 404 || err.status === 500) {
-                    file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-                } else {
-                    res.writeHead(err.status, err.headers);
-                    res.end();
-                }
-            } else {
-                res.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});
 
-            }
-        });
-    }
-}).listen(portNo);
+      else {
+          file.serve(req, res, function (err, result) {
+              if (err != null) {
+                  console.error('Error serving %s - %s', req.url, err.message);
+                  if (err.status === 404 || err.status === 500) {
+                      file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+                  } else {
+                      res.writeHead(err.status, err.headers);
+                      res.end();
+                  }
+              } else {
+                  res.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});
+
+              }
+          });
+      }
+  }).listen(portNo);
