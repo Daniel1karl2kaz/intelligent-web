6fa7293a55e07bc99712046edea2fbfcba742ae2
Daniel1karl2kaz <danielayeni1995@gmail.com>
2016-04-03 19:14:16 +0100

    Added files via upload

--- server.js
+++ server.js
@@ -1,78 +1,113 @@
-/**
- * Created by fabio on 18/02/15.
- */
 var protocol = require('http');
 var static = require('node-static');
 var util = require('util');
 var url = require('url');
 var querystring = require('querystring');
 var Twit = require('twit');
+var mysql = require('mysql');
+
 
 
 var client = new Twit({
-  consumer_key: 'p3z3Uya200JQo6itVOOWixkW8',
-  consumer_secret: 'wXMbKhs2OVsCi5UUQaJAIHo2fLR2KGy42FbzZ8vCWsPqsXxBIg',
-  access_token: '277462049-YNaK9sdOazCkG6XrltVWUgynV1olfnMlqPfKTDQm',
-  access_token_secret: 'hQyUBW1MGd2fryHinjqQNrQZjybsoSw5zevwLxIaicHgo'
+  consumer_key: 'QQY9a1KOfB9f9yrDu5TPYNxM1',
+  consumer_secret: 'KFcrYRVEAou07gQwrjNUmHUsJInLa7kz8TdeCMaxSjC3sD0EEM',
+  access_token: '312094689-JpmZ9EJwxUUmZErdzTbagA5zlnndyyXj4ckOVQtE',
+  access_token_secret: 'S6U36toKMU9FZNriCh2MqM4ZbYzbnzQ65l8w6m0ydeqEe'
 });
 
- var file = new (static.Server)();
-  var portNo = 3001;
-  var app = protocol.createServer(function (req, res) {
-      var pathname = url.parse(req.url).pathname;
-      if ((req.method == 'POST') && (pathname == '/postFile.html')) {
-          var body = '';
-          req.on('data', function (data) {
-              body += data;
-              // if body >  1e6 === 1 * Math.pow(10, 6) ~~~ 1MB
-              // flood attack or faulty client
-              // (code 413: req entity too large), kill req
-              if (body.length > 1e6) {
-                  res.writeHead(413,
-                      {'Content-Type': 'text/plain'}).end();
-                  req.connection.destroy();
-              }
-          });
-          req.on('end', function () {
-              var string = JSON.parse(body);
-              res.writeHead(200, {"Content-Type": "application/json"});
-             //res.write(body);
+// var connection = mysql.createConnection(
+//     {
+//       host     : 'stusql.dcs.shef.ac.uk',
+//         port     : '3306',
+//         user     : 'team086',
+//         password : 'c8f56171',
+//         database : 'team086'
+//
+//     }
+// );
+//
+// connection.connect();
 
-              jsonx = {};
+var file = new (static.Server)();
+var portNo = 3001;
+var app = protocol.createServer(function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  if ((req.method == 'POST') && (pathname == '/postFile.html')) {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+      // if body >  1e6 === 1 * Math.pow(10, 6) ~~~ 1MB
+      // flood attack or faulty client
+      // (code 413: req entity too large), kill req
+      if (body.length > 1e6) {
+        res.writeHead(413,
+          {'Content-Type': 'text/plain'}).end();
+          req.connection.destroy();
+        }
+      });
+      req.on('end', function () {
+        var string = JSON.parse(body);
+        res.writeHead(200, {"Content-Type": "application/json"});
+
+        if (string.checktweet1 == 'on') {
+          tweets(string.teamname)
+      }
+      if (string.checkmentions1 == 'on'){
+         mentions(string.teamname)
+      }
+      if (string.checktweet2 == 'on'){
+        tweets(string.playername)
+      }
+
+      if (string.checkmentions2 == 'on'){
+          mentions(string.playername)
+      }
+
+   });
+
+   function mentions(x){
+     client.get('search/tweets', {q:"@"+x, count:1},
+      function(err,data,response){
+       for(var index in data.statuses){
+         var tweet = data.statuses[index];
+          console.log(tweet.text);
+          console.log(" ")
 
 
-              client.get('search/tweets', { q:string.teamname, count:13},
-              function searchTweets(err, data, response) {
-                for(var index in data.statuses){
-                  var tweet = data.statuses[index];
-
-
-                  jsonx[index] = tweet;
-
-
-                }
-                console.log(jsonx);
-                res.end(JSON.stringify(jsonx));
-              });
-
-        });
+       }
+     })
    }
 
+   function tweets(y){
+     client.get('statuses/user_timeline', {screen_name:"@"+y, count:1},
+       function(err,data,response) {
+         for(var index in data){
+             var tweet = data[index];
+             console.log(tweet.text);
+             console.log(" ")
 
-      else {
-          file.serve(req, res, function (err, result) {
-              if (err != null) {
-                  console.error('Error serving %s - %s', req.url, err.message);
-                  if (err.status === 404 || err.status === 500) {
-                      file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-                  } else {
-                      res.writeHead(err.status, err.headers);
-                      res.end();
-                  }
-              } else {
-                  res.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});
+         }
 
-              }
-          });
-      }
+       })
+
+   }
+
+    }
+
+    else {
+      file.serve(req, res, function (err, result) {
+        if (err != null) {
+          console.error('Error serving %s - %s', req.url, err.message);
+          if (err.status === 404 || err.status === 500) {
+            file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+          } else {
+            res.writeHead(err.status, err.headers);
+            res.end();
+          }
+        } else {
+          res.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});
+
+        }
+      });
+    }
   }).listen(portNo);
